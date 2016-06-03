627b9641a07c46d9a6c99bf64a42cb94e15c76bd
Robert Acock <ThisUsedToBeAnEmail@gmail.com>
2016-06-03 13:52:54 +0100

    generate searchable POD, well it works for MOO, MOOSE and HTML::SocialMeta ;)

--- lib/Pod/Hashed.pm
+++ lib/Pod/Hashed.pm
@@ -33,22 +33,17 @@ has 'section' => (
 
 sub run  {
     my $self = shift;
-    
+
     my ($tag, $text, $identifier, $title, $para);
-    my %valid_tags = ( head1 => 1, head2 => 1,  Para => 1,  item => 1);
+    my %valid_tags = ( 'head1' => 1, 'head2' => 1,  'Para' => 1, 'over-text' => 1, 'item-text' => 1, 'Verbatim' => 1);
+    my %text_tags = (  'Para' => 1,  'item-text' => 1, 'over-text' => 1, 'Verbatim' => 1, 'C' => 1, 'L' => 1, 'B' => 1, 'I' => 1 );
     while ( my $token = $self->get_token ) {
         if ( $token->isa('Pod::Simple::PullParserStartToken') ) {    
-            if (exists $valid_tags{$token->tagname}) {
-                $tag = $token->tagname;
-                
-                next if $tag eq q{Para};
+            $tag = $token->tagname;
+            if (exists $valid_tags{$tag}) {
+                next if exists $text_tags{$tag};
 
-                if (!$identifier) { 
-                    # set the identifier on the first tag
-                    $identifier = $tag;
-                }
-
-                if ($title && $tag){
+                if ($title && $identifier){
                     $self->set_section(
                         identifier => $identifier,
                         title => $title, 
@@ -58,20 +53,48 @@ sub run  {
                     $self->insert_pod;
                     ($title, $para, $identifier) = undef;
                 }
+
+                if (!$identifier) { 
+                    # set the identifier on the first tag
+                    $identifier = $tag;
+                }
             }
         } 
         elsif ( $token->isa('Pod::Simple::PullParserTextToken') ) {
             $text = $token->text;
-        } 
-        elsif ( $token->isa('Pod::Simple::PullParserEndToken') ) {
-            if ( $tag && $text ) { 
-                if ($tag eq 'Para') {
-                    $para = $para ? $para . "\n" . $text : $text;
-                } else {
-                    $title = $text;
+            if (exists $text_tags{$tag}) {        
+                if ($para) {
+                    if ($tag =~ m{item-text|over-text}){
+                        $para = $para . "\n\n" . $text . "\n\n";
+                    }
+                    elsif ($para =~ /[\.\;\:]$/) {
+                        $para = $para . "\n\n" . $text;
+                    } 
+                    else {
+                        $para = $para . $text;
+                    }
+                } 
+                else {
+                    $para = $text;
                 }
+            } 
+            else {
+                $title = $text;
             }
         } 
+        elsif ( $token->isa('Pod::Simple::PullParserEndToken') ) {
+        
+        } 
+    }
+
+    if ($title && $identifier){
+        $self->set_section(
+            identifier => $identifier,
+            title => $title, 
+            content => $para
+        );
+
+        $self->insert_pod;
     }
 }
 
